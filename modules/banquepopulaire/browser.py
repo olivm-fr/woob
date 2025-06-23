376b5545ed595a18d5dea839b56ecd190a0e620b
eraby <etienne.raby@nokia.com>
2025-06-23 09:12:18 +0200

    fix pipeline

--- modules/banquepopulaire/browser.py
+++ modules/banquepopulaire/browser.py
@@ -276,7 +276,7 @@ class BanquePopulaire(TwoFactorBrowser):
 
         try:
             headers = {
-                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",  # Mandatory, else you've got an HTML page.
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
                 "Referer": "https://www.banquepopulaire.fr/",
             }
             self.new_first_login_page.go(headers=headers, params={"service": "cyber"})
