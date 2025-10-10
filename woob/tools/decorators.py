b7979af9940faeaaf26fbd46aebbdde6a3e09ad1
Gilles Dartiguelongue <gilles.dartiguelongue@esiee.org>
2025-10-10 11:33:02 +0200

    chore: add type annotations to woob.tools.decorators
    It is currently not possible to use ParamSpec and inject new optional
    keyword arguments. This is unfortunate but it should not be a problem as
    the feature to inject `_tries` or `_delay` at function call site does
    not appear to be used anywhere. Choosing to favor full type checking
    over supporting a rarely used feature for now.
    
    Refs: https://stackoverflow.com/questions/79679286/how-to-use-a-decorator-to-mark-a-keyword-only-mandatory-argument-as-optional-in

--- woob/tools/decorators.py
+++ woob/tools/decorators.py
@@ -15,24 +15,40 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with woob. If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import annotations
+
 import logging
 import time
+from typing import Any, Callable, Protocol, TypeVar, cast
 
 
 __all__ = ["retry"]
 
+R = TypeVar("R")
 
-def retry(exceptions_to_check, exc_handler=None, tries=3, delay=2, backoff=2):
+
+class ExceptionHandler(Protocol):
+    def __call__(self, exc: BaseException, **kwargs: Any) -> None: ...
+
+
+# TODO: Revisit with ParamSpec
+def retry(
+    exceptions_to_check: type[BaseException] | tuple[type[BaseException], ...],
+    exc_handler: ExceptionHandler | None = None,
+    tries: int = 3,
+    delay: float = 2,
+    backoff: float = 2,
+) -> Callable[[Callable[..., R]], Callable[..., R]]:
     """
     Retry decorator
     from https://www.saltycrane.com/blog/2009/11/trying-out-retry-decorator-python/
     original from https://wiki.python.org/moin/PythonDecoratorLibrary#Retry
     """
 
-    def deco_retry(f):
-        def f_retry(*args, **kwargs):
-            mtries = kwargs.pop("_tries", tries)
-            mdelay = kwargs.pop("_delay", delay)
+    def deco_retry(f: Callable[..., R]) -> Callable[..., R]:
+        def f_retry(*args: Any, **kwargs: Any) -> R:
+            mtries = cast(int, kwargs.pop("_tries", tries))
+            mdelay = cast(float, kwargs.pop("_delay", delay))
             while mtries > 1:
                 try:
                     return f(*args, **kwargs)
