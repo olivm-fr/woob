85519db15bc4de215e697a50b0432cdaf42ba3ab
Ludovic LANGE <llange@users.noreply.github.com>
2025-11-04 22:02:30 +0100

    [ameli] Add (more complete) CA certificate chain to prevent SSL errors
    It seems that the CA certificates (intermediary / root) are not part of all
    the cacert packages nor browser, resulting in SSL errors preventing the
    proper communication.
    These were retrieved from https://www.certigna.com/autorites-de-certification/ for ameliconnect.ameli.fr:
    * Certigna Services CA 07F2CE55CA1AA6CB992719B1E423C1D02C1EA759A6E2EAB4E150C88282E22550
    * Certigna Root CA D48D3D23EEDB50A459E55197601C27774B9D7B18C94D5A059511A10250B93168
    
        (converted from DER to PEM + concatenated to make a proper chain)
    
    And for assure.ameli.fr:
    * Sectigo Public Server Authentication Root R46 (https://crt.sh/?d=4256644734)
    * Sectigo Public Server Authentication CA OV R36 (https://crt.sh/?d=4267304698)

--- modules/ameli/browser.py
+++ modules/ameli/browser.py
@@ -105,7 +105,7 @@ class AmeliBrowser(TwoFactorBrowser):
     STATE_DURATION = 15
     __states__ = ("otp_form_data", "otp_form_url", "trust_connect")
 
-    VERIFY = "certigna.pem"
+    VERIFY = "ameli.pem"
 
     def __init__(self, config, *args, **kwargs):
         super().__init__(config, *args, **kwargs)
