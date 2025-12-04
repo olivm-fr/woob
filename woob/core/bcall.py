eabf193df53cb7aa2260adafcdc95b16ab29b933
Gilles Dartiguelongue <gilles.dartiguelongue@esiee.org>
2025-12-04 16:26:05 +0100

    chore: Add type annotations to woob.core.bcall

--- woob/core/bcall.py
+++ woob/core/bcall.py
@@ -15,21 +15,27 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with woob. If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import annotations
 
 import queue
+from collections.abc import Iterable, Iterator
 from copy import copy
 from threading import Event, Thread
+from typing import Any, Callable
 
 from woob.capabilities.base import BaseObject
+from woob.tools.backend import Module
 from woob.tools.log import getLogger
 from woob.tools.misc import get_backtrace
 
 
 __all__ = ["BackendsCall", "CallErrors"]
 
+CallError = tuple[Module, Exception, str]
+
 
 class CallErrors(Exception):
-    def __init__(self, errors):
+    def __init__(self, errors: Iterable[CallError]) -> None:
         msg = "Errors during backend calls:\n" + "\n".join(
             [f"Module({backend!r}): {error!r}\n{backtrace!r}\n" for backend, error, backtrace in errors]
         )
@@ -37,23 +43,23 @@ class CallErrors(Exception):
         super().__init__(msg)
         self.errors = copy(errors)
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[CallError]:
         return self.errors.__iter__()
 
 
 class BackendsCall:
-    def __init__(self, backends, function, *args, **kwargs):
+    def __init__(
+        self, backends: Iterable[Module], function: str | Callable[..., Any], *args: Any, **kwargs: Any
+    ) -> None:
         """
         :param backends: List of backends to call
-        :type backends: list[:class:`Module`]
         :param function: backends' method name, or callable object.
-        :type function: :class:`str` or :class:`callable`
         """
         self.logger = getLogger(__name__)
 
-        self.responses = queue.Queue()
-        self.errors = []
-        self.tasks = queue.Queue()
+        self.responses: queue.Queue[Any] = queue.Queue()
+        self.errors: list[CallError] = []
+        self.tasks: queue.Queue[Module] = queue.Queue()
         self.stop_event = Event()
         self.threads = []
 
@@ -63,7 +69,7 @@ class BackendsCall:
             self.threads.append(t)
             self.tasks.put(backend)
 
-    def store_result(self, backend, result):
+    def store_result(self, backend: Module, result: Any) -> None:
         """Store the result when a backend task finished."""
         if result is None:
             return
@@ -72,7 +78,12 @@ class BackendsCall:
             result.backend = backend.name
         self.responses.put(result)
 
-    def backend_process(self, function, args, kwargs):
+    def backend_process(
+        self,
+        function: str | Callable[..., Any],
+        args: Any,
+        kwargs: Any,
+    ) -> None:
         """
         Internal method to run a method of a backend.
 
@@ -90,7 +101,7 @@ class BackendsCall:
                         result = getattr(backend, function)(*args, **kwargs)
                 except Exception as error:
                     self.logger.debug("%s: Called function %s raised an error: %r", backend, function, error)
-                    self.errors.append((backend, error, get_backtrace(error)))
+                    self.errors.append((backend, error, get_backtrace(str(error))))
                 else:
                     self.logger.debug("%s: Called function %s returned: %r", backend, function, result)
 
@@ -102,13 +113,18 @@ class BackendsCall:
                                 if self.stop_event.is_set():
                                     break
                         except Exception as error:
-                            self.errors.append((backend, error, get_backtrace(error)))
+                            self.errors.append((backend, error, get_backtrace(str(error))))
                     else:
                         self.store_result(backend, result)
             finally:
                 self.tasks.task_done()
 
-    def _callback_thread_run(self, callback, errback, finishback):
+    def _callback_thread_run(
+        self,
+        callback: Callable[[Any], None] | None,
+        errback: Callable[[Module, Exception, str], None] | None,
+        finishback: Callable[[], None] | None,
+    ) -> None:
         while not self.stop_event.is_set() and (self.tasks.unfinished_tasks or not self.responses.empty()):
             try:
                 response = self.responses.get(timeout=0.1)
@@ -125,7 +141,12 @@ class BackendsCall:
         if finishback:
             finishback()
 
-    def callback_thread(self, callback, errback=None, finishback=None):
+    def callback_thread(
+        self,
+        callback: Callable[[Any], None] | None,
+        errback: Callable[[Module, Exception, str], None] | None = None,
+        finishback: Callable[[], None] | None = None,
+    ) -> Thread:
         """
         Call this method to create a thread which will callback a
         specified function everytimes a new result comes.
@@ -143,7 +164,7 @@ class BackendsCall:
         thread.start()
         return thread
 
-    def wait(self):
+    def wait(self) -> None:
         """Wait until all tasks are finished."""
         for thread in self.threads:
             thread.join()
@@ -151,12 +172,11 @@ class BackendsCall:
         if self.errors:
             raise CallErrors(self.errors)
 
-    def stop(self, wait=False):
+    def stop(self, wait: bool = False) -> None:
         """
         Stop all tasks.
 
         :param wait: If True, wait until all tasks stopped.
-        :type wait: bool
         """
 
         self.stop_event.set()
@@ -164,7 +184,7 @@ class BackendsCall:
         if wait:
             self.wait()
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[Any]:
         try:
             while not self.stop_event.is_set() and (self.tasks.unfinished_tasks or not self.responses.empty()):
                 try:
